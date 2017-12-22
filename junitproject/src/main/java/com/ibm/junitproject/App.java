bec48420250d437fadf45ac1b2c59eb55c5cac6f
3370_30310 <3370_30310@noemail.com>
2017-12-22 15:12:56 +0530

    initial commit

---
+++ junitproject/src/main/java/com/ibm/junitproject/App.java
@@ -0,0 +1,17 @@
+package com.ibm.junitproject;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println(StringUtils.capitalize("Helloworld"));
+    	System.out.println(StringUtils.deleteWhitespace("He ll o w orld"));
+        System.out.println( "Hello World!" );
+    }
+}
